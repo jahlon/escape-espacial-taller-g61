2240bab266a50746285785223dad4a98a7b9392b
jahlon <jahlon@gmail.com>
2018-10-24 17:19:40 -0500

    Se agregó la funcionalidad para generar las estrellas en el mundo y moverlas

--- Estrella.java
+++ Estrella.java
@@ -1,18 +1,27 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class Estrella extends Actor {
-    
+
     private int velocidad;
+
+    public Estrella() {
+        GreenfootImage imagen = new GreenfootImage(6,6);
+        imagen.setColor(Color.WHITE);
+        imagen.drawLine(0, imagen.getHeight()/2, imagen.getWidth(), imagen.getHeight()/2);
+        imagen.drawLine(imagen.getWidth()/2, 0, imagen.getWidth()/2, imagen.getHeight());
+        setImage(imagen);
+        
+        velocidad = Greenfoot.getRandomNumber(8) + 3;
+    }
     
-	public Estrella() {
-		GreenfootImage imagen = new GreenfootImage(6,6);
-		imagen.setColor(Color.WHITE);
-		imagen.drawLine(0, imagen.getHeight()/2, imagen.getWidth(), imagen.getHeight()/2);
-		imagen.drawLine(imagen.getWidth()/2, 0, imagen.getWidth()/2, imagen.getHeight());
-		setImage(imagen);
-	}
-	
+    public void mover() {
+        setLocation(getX(), getY() + velocidad);
+    }
+
     public void act() {
-        // Add your action code here.
+        mover();
+        if( getY() >= getWorld().getHeight() ) {
+            getWorld().removeObject(this);
+        }
     }    
 }
