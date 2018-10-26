d8ec43611af069960205706911f2afbf95fe0cbc
jessica2620 <jessica-bilbao@hotmail.com>
2018-10-26 17:39:36 -0500

    Se creó la animación de la explosión en la clase explosión y se modificó la clase nave para realizar el requisito destruir nave

--- Explosion.java
+++ Explosion.java
@@ -1,10 +1,39 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class Explosion extends Actor {
-    
+    public static final int TOTAL_IMAGENES = 25;
+    public static final int ITERACIONES_ANIMACION = 10;
     private GreenfootSound sonido;
-    
+    private GreenfootImage[] imagenes;
+    private int indiceImagenes;
+    private int contadorIteraciones;
+
+    public Explosion(){
+        imagenes = new GreenfootImage[TOTAL_IMAGENES];
+
+        for(int i = 0; i < TOTAL_IMAGENES; i++){
+            imagenes[i] = new GreenfootImage("explosion/explosion" + i + ".png");
+
+        }
+        setImage(imagenes[0]);
+    }
+
+    public void animar(){
+        if(contadorIteraciones >= ITERACIONES_ANIMACION){
+
+            if (indiceImagenes < TOTAL_IMAGENES - 1){           
+                indiceImagenes++;
+            } else{
+                indiceImagenes = 0;            
+            }
+            setImage(imagenes[indiceImagenes]);
+            contadorIteraciones = 0;
+        }else {
+            contadorIteraciones++;
+        }
+    }
+
     public void act() {
-        
+        animar();
     }    
 }
--- Nave.java
+++ Nave.java
@@ -1,11 +1,27 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class Nave extends Actor {
-    
+
     public static final int VELOCIDAD = 5;
-    
+
     private Explosion explosion;
-    
+
+    public void explotarNave() {
+        if (isTouching(Asteroide.class)) {
+            int Y = getY();
+            int X = getX();
+
+            getImage().clear();
+
+            explosion = new Explosion();
+            explosion.setLocation(X,Y);
+            explosion.animar();
+
+            getWorld().removeObject(this);
+        }
+    }
+
     public void act() {
+        explotarNave();
     }    
 }
