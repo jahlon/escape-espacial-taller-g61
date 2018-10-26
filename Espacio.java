320804c3626331fafb711c669f4cc1a73334140a
santiago0923V <santy923@hotmail.com>
2018-10-26 17:14:35 -0500

    se agregó el requerimiento terminar juego

--- Espacio.java
+++ Espacio.java
@@ -4,14 +4,18 @@ public class Espacio extends World {
 
     private GreenfootSound sonidoDeFondo;
     private int tiempoJuego;
-    private int tiempoGeneracionAsteroides;
-
+    private int tiempoGeneracionAsteroides; 
     private Nave nave;
     private Counter contador;
     private Boton botonRetry;
+    boolean sonarMusica;
+    boolean pararJuego;
 
     public Espacio() {    
         super(800, 600, 1,false);
+        botonRetry= new Boton();
+        pararJuego=true;
+        sonarMusica=true;
 
         // Configurar fondo negro
         GreenfootImage fondo = getBackground();
@@ -27,6 +31,16 @@ public class Espacio extends World {
         int x = Greenfoot.getRandomNumber(getWidth());
         int y = -5;
         addObject(estrella, x, y);
+    }
+
+    public void terminarJuego(){
+
+        int x= getWidth();
+        int y=getHeight();
+        pararJuego=false;
+        sonarMusica=false;
+        addObject(botonRetry,x/2,y/2);
+
     }
     
     public void act() {
